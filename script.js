77718bed1d491e0fbbaa706c5593db37f214eb3a
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-02 20:29:01 -0800

    Add methods

--- script.js
+++ script.js
@@ -23,22 +23,23 @@ class Code {
         ]
       );
     }
-    return pegs;
+    console.log(new Code(pegs));
+    return new Code(pegs);
   }
 
   static fromString(str) {
     return new Code(str.split(''));
   }
 
-  numExactMatches(code) {
+  numExactMatches = code => {
     let exactMatches = 0;
     for (let i = 0; i < this.pegs.length; i++) {
       if (code.pegs[i] === this.pegs[i]) exactMatches++;
     }
     return exactMatches;
-  }
+  };
 
-  numNearMatches(code) {
+  numNearMatches = code => {
     let nearMatches = 0;
     for (let i = 0; i < this.pegs.length; i++) {
       const codeColor = code.pegs[i];
@@ -46,12 +47,50 @@ class Code {
         nearMatches++;
     }
     return nearMatches;
+  };
+
+  exactMatches = code => {
+    return JSON.stringify(code.pegs) === JSON.stringify(this.pegs);
+  };
+}
+
+class MasterMind {
+  constructor(length) {
+    console.log(Code.random(length));
+    this.secretCode = Code.random(length);
+    // this.secretCode.pegs = ['B', 'B', 'B', 'B'];
+    console.log('Secret code', { secret: this.secretCode.pegs });
+    this.startGame();
   }
 
-  exactMatches(code) {
-    return JSON.stringify(code.pegs) === JSON.stringify(this.pegs);
+  startGame() {
+    let gameOver = false;
+    while (!gameOver) {
+      gameOver = this.askUserForGuess();
+    }
+    console.log('you win!');
+  }
+
+  printMatches(code) {
+    // console.log(code);
+    // const exactMatches = this.secretCode.numNearMatches(code);
+    const exactMatches = code.numExactMatches(this.secretCode);
+    const nearMatches = code.numNearMatches(this.secretCode);
+
+    console.log(`Exact matches: ${exactMatches}`);
+    console.log(`Near matches: ${nearMatches}`);
+  }
+
+  askUserForGuess() {
+    const input = prompt('Enter a code');
+    console.log(`Your guess is: ${input}`);
+
+    let inputCode = Code.fromString(input);
+    this.printMatches(inputCode);
+    return inputCode.exactMatches(this.secretCode);
   }
 }
+
 /*
 let code = new Code(['R', 'G', 'R', 'B']);
 console.log(JSON.stringify(code));
@@ -67,6 +106,7 @@ console.log(Code.random(5));
 console.log(Code.fromString('GBGB'));
 */
 
+/*
 let code = new Code(['R', 'G', 'R', 'B']);
 console.log(code.numExactMatches(new Code(['R', 'Y', 'Y', 'B'])));
 console.log(code.numExactMatches(new Code(['Y', 'Y', 'Y', 'B'])));
@@ -80,3 +120,17 @@ console.log(code.exactMatches(new Code(['R', 'G', 'R', 'B'])));
 console.log(code.exactMatches(new Code(['R', 'G', 'Y', 'Y'])));
 console.log(code.exactMatches(new Code(['B', 'R', 'Y', 'Y'])));
 console.log(code.exactMatches(new Code(['R', 'G', 'R', 'B', 'B'])));
+*/
+
+/*
+let masterMind = new MasterMind(4);
+console.log(JSON.stringify(masterMind));
+masterMind = new MasterMind(5);
+console.log(JSON.stringify(masterMind));
+*/
+
+let mastermind = new MasterMind(4);
+// mastermind.secretCode = ['R', 'G', 'R', 'B'];
+// console.log(JSON.stringify(mastermind));
+// mastermind.printMatches(new Code(['R', 'Y', 'Y', 'B'])); //2
+// mastermind.printMatches(new Code(['Y', 'Y', 'Y', 'G'])); //1
