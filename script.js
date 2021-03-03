768017e58a06432308410a36b3a12f2e93a40e23
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-02 19:04:19 -0800

    random pegs

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const POSSIBLE_PEGS = { R: 'red', G: 'green', B: 'blue', Y: 'yellow' };
+const POSSIBLE_PEGS = { B: 'blue', R: 'red', Y: 'yellow', G: 'green' };
 class Code {
-  static constructor() {}
+  constructor(pegs) {
+    if (Code.areValidPegs(pegs)) {
+      this.pegs = pegs.map(peg => peg.toUpperCase());
+    } else {
+      throw new Error('Invalid pegs');
+    }
+  }
 
   static areValidPegs(pegs) {
     return pegs.every(peg => {
       return Object.keys(POSSIBLE_PEGS).includes(peg.toUpperCase());
     });
   }
+
+  static random(length) {
+    let pegs = [];
+    for (let i = 0; i < length; i++) {
+      pegs.push(
+        Object.keys(POSSIBLE_PEGS)[
+          Math.floor(Math.random() * Object.keys(POSSIBLE_PEGS).length)
+        ]
+      );
+    }
+    return pegs;
+  }
 }
 
 let code = new Code(['R', 'G', 'R', 'B']);
 console.log(JSON.stringify(code));
-
+// code = new Code(['R', 'G', 'z', 'B']);
 console.log(Code.areValidPegs(['B', 'Y', 'G', 'G']));
 console.log(Code.areValidPegs(['b', 'y', 'g', 'g']));
 console.log(Code.areValidPegs(['B', 'Y', 'Z', 'G']));
+let code2 = new Code(['r', 'g', 'r', 'b']);
+console.log(JSON.stringify(code2));
+console.log(Code.random(5));
+console.log(Code.random(5));
+console.log(Code.random(5));
